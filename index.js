d8feb7d6fd03817e573bf6ef94d9c89d56f7da93
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-01-18 10:21:33 +0200

    Feat: handle division 0 edge case

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function addition(num1, num2) {
-    console.log(num1 + num2);
+    return num1 + num2;
 }
 
 function subtraction(num1, num2) {
-    console.log(num1 - num2);
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if(num2 === 0) {
+        return "LOL";
+    }
+    return num1 / num2;
 }
