9967c76388ead8a1090e1c78f1d9f11fdae934b0
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-01-18 10:44:44 +0200

    Implement operate function

--- index.js
+++ index.js
@@ -27,3 +27,18 @@ function division(num1, num2) {
     return num1 / num2;
 }
 
+
+function operate(num1, num2, operator) {
+    if(operator === add) {
+        return addition(num1, num2);
+
+    } else if(operator === subtract) {
+        return subtraction(num1, num2);
+
+    } else if(operator === multiply) {
+        return multiplication(num1, num2);
+
+    } else if(operator === divide) {
+        return division(num1, num2)
+    }
+}
