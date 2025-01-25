3d6bb49384b5ed101f045eda1604b3e863fa9977
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-01-25 12:56:43 +0200

    Add global variables comment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let num1;
+let num1;  // declared globally to access later
 let num2;
 
 let add = '+';
@@ -42,3 +42,5 @@ function operate(num1, num2, operator) {
         return division(num1, num2)
     }
 }
+
+
